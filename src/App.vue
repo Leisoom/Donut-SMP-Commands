1175d577dfa47e4ea8e573f9ba4824003d17901c
Leissom <leisoom.gg@gmail.com>
2026-02-22 21:39:25 -0800

    use router link

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <v-app>
     <v-app-bar density="compact" elevation="0">
-      <v-app-bar-title><a class="text-decoration-none font-weight-bold text-secondary" href="/">Donut Commands</a></v-app-bar-title>
-      <v-btn
+      <v-app-bar-title><router-link class="text-decoration-none font-weight-bold text-secondary" to="/">Donut Commands</router-link></v-app-bar-title>
+      <router-link
         class="text-body-1"
         :ripple="false"
         to="/AuctionGuide"
         variant="plain"
       >
         Auction Guide
-      </v-btn>
+      </router-link>
     </v-app-bar>
     <v-main>
       <router-view />
