cc7af371acb02cb02a3a5278ec9008b452e1a801
Leissom <leisoom.gg@gmail.com>
2026-02-22 21:12:56 -0800

    configure for github pages

--- src/components.d.ts
+++ src/components.d.ts
@@ -12,7 +12,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     Command: typeof import('./components/Command.vue')['default']
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
